63da924e6f6198f078a60d866faa0c1801054d09
nnachi_chima <chimakingsley216@gmail.com>
2022-07-03 15:43:16 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - printing all single digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar("0123456789\n");
+	return (0);
+}
