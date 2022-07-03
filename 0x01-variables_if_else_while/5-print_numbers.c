46f0a94fd234b044f8d9bf31db7cd26b09f154c2
nnachi_chima <chimakingsley216@gmail.com>
2022-07-03 08:40:04 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Description - printd all single number of base 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char base;
+
+	while (base < 10)
+	{
+		purchar(base);
+		base++;
+	}
+	purchar("\n");
+	return (0);
+}
