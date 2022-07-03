1aaee1b98a843bb65c1f801ba506b4e521941ecc
nnachi_chima <chimakingsley216@gmail.com>
2022-07-03 08:28:28 -0700

    print_alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - print exception
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char low;
+
+	low = 'a';
+	while (l <= 'z')
+	{
+		if ((low != 'e') && (low != 'q'))
+		{
+			putchar(low);
+			low;
+		}
+		else
+		{
+			low++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
