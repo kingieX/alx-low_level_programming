91246a2641f2c4e5f2774e3f222913262d85f9b3
nnachi_chima <chimakingsley216@gmail.com>
2022-07-05 11:45:11 -0700

    function nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - prints 10 times the alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	count = 1;
+	char letter;
+
+	while (count++ <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
