e1df1c2d7166ace64b27201286ee70bd7acd3bb3
nnachi_chima <chimakingsley216@gmail.com>
2022-07-05 12:19:32 -0700

    function nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description - checks for lowercase character
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	else
+	}
+		return (0);
+}
