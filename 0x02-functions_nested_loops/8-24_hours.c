72e884560c5eda8bc4a10251a72b18e4b6582808
nnachi_chima <chimakingsley216@gmail.com>
2022-07-06 03:09:44 -0700

    function nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minite of the day of jack bauer
+ *
+ */
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
