5377e69cfd7cd3f8e2f43f5b4799cb6beca992b4
nnachi_chima <chimakingsley216@gmail.com>
2022-07-06 06:30:45 -0700

    function nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d,", n--);
-		printf("%d\n", n);
+		printf("%d ", n);
 	}
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-		printf("%d\n", n);
+		printf("%d ", n);
 	}
 }
