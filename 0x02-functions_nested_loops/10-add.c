caff60fdde8711f8ba0314fd59fd888c17728072
nnachi_chima <chimakingsley216@gmail.com>
2022-07-06 04:25:37 -0700

    function nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the results
- *@a - input
- @b - input
+ *int @a - input
+ *@int b - input
  * Return: result
  */
 int add(int a, int b)
