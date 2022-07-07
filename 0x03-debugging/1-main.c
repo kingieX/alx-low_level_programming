dde60d10cf43e7eacae70ab49eaecae318025a93
nnachi_chima <chimakingsley216@gmail.com>
2022-07-07 15:32:54 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf('infinte loop incoming :'\n');
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
