6c5223d27495d8a496c2e8c68d0ed87c6a7fe33d
nnachi_chima <chimakingsley216@gmail.com>
2022-07-12 09:24:58 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main."
+/**
+ * puts2 - prints every other character of a string
+ * @str
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
