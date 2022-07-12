48dd97491e4dcf488358f8f8e3406f820804da99
nnachi_chima <chimakingsley216@gmail.com>
2022-07-12 09:41:35 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: peremeter
+ */
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
