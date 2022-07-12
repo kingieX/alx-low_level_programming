ba966071983b4646aafe9c2c5b54d024ed96f991
nnachi_chima <chimakingsley216@gmail.com>
2022-07-12 07:27:22 -0700

    pointer, array and string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  *_strlen - get the length of a string
  *@s: string pointer to pass to this function
+ * Return: The length of *s
  */
 int _strlen(char *s)
 {
 	size_t length = 0;
-	
+
 	while (*s++)
 		length++;
 
-	return(length);
+	return (length);
 }
