ec31e5a5718042dbff7aa120c4c4bac1ac3b88a5
nnachi_chima <chimakingsley216@gmail.com>
2022-07-14 05:59:35 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: first paremeter
+ * @src: second parameter
+ * Return: a character
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += i;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
