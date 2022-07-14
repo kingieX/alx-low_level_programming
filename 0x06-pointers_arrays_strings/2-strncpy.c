f4ef9e58bb12b0bb2e733beb747f063def34039b
nnachi_chima <chimakingsley216@gmail.com>
2022-07-14 07:00:24 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncpy - function that copies a string
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
