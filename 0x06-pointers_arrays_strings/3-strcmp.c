4b5e6d3dead4c595d815f592b0375e42e51ab00a
nnachi_chima <chimakingsley216@gmail.com>
2022-07-14 07:12:35 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function that compares two strings
+ * @s1: 1st parameter
+ * @s2: 2nd parameter
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
