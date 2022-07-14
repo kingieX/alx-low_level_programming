783de358d484b09b80ae5b3459f309744b64e2b4
nnachi_chima <chimakingsley216@gmail.com>
2022-07-14 06:51:04 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,8 +3,11 @@
  * *_strncat - concatenates two strings
  * @dest: first parameter
  * @src: second parameter
+ * @n: third parameter
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
+	strncat(dest, src, n);
+	return (dest);
+}
