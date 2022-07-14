04f0b0e2c18127593704aa38690fac3e903e3e99
nnachi_chima <chimakingsley216@gmail.com>
2022-07-14 07:43:14 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - convert any sring string to uppercase
+ * @k: string parameter
+ * Return: string
+ */
+char *string_toupper(char *k)
+{
+	int i = 0;
+
+	while (k[i])
+	{
+		if (k[i] >= 97 && k[k] <= 122)
+			k[i] = k[i] - 32;
+		i++;
+	}
+	return (k);
+}
