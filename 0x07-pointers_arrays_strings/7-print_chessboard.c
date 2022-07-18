4c031160836758685d727d078856fcfd88069ac4
nnachi_chima <chimakingsley216@gmail.com>
2022-07-18 09:39:28 -0700

    even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar('\0');
+	}
+}
+
