ed5a59b5b0e74b5c695f811338e4dcb2756ea2ee
nnachi_chima <chimakingsley216@gmail.com>
2022-07-18 09:26:10 -0700

    even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a string
+ * @haystack: string
+ * @needle: string
+ * Return: substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, a = 0;
+
+	while (haystack[i])
+	{
+		while (needle[a] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + a] == needle[a])
+				a++;
+			else
+				break;
+		}
+		if (needle[a])
+		{
+			i++;
+			a = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
