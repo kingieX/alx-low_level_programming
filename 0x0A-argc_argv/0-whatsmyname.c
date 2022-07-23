ed57e33df084a8e613042cc4337b7549ada582eb
nnachi_chima <chimakingsley216@gmail.com>
2022-07-23 04:06:44 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: argv parameter
  * Return: 0
  */
-int main(int argc __attribute __((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
