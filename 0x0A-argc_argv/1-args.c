1207236efde2ddf9394ade12e8644fc456c24ec8
nnachi_chima <chimakingsley216@gmail.com>
2022-07-23 04:55:06 -0700

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that prints the number of arguments passed into it.
+ * @argc: argument
+ * @argv: vector argument
+ * Return: success
+ */
+int main(int argc, char *argv[] __attributr__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
