48e7473bd738d1fb6a718028a98a61fb309ce128
nnachi_chima <chimakingsley216@gmail.com>
2022-07-23 03:27:42 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that prints its name, followed by a new line.
+ * @argc: argc parameter
+ * @argv: argv parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+		return (0);
+}
