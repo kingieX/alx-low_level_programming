76533982c72aa197f2ec89a1c814186188df45a8
nnachi_chima <chimakingsley216@gmail.com>
2022-07-28 07:52:20 -0700

    more malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a;
-	unsigned int z;
+	unsigned int i;
 
 	if (nmemb == 0 || size size == 0)
 		return (NULL);
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
-	for (z = 0; z < (nmemb * size); z++)
-		a[z] = 0
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0
 			return (a);
 }
