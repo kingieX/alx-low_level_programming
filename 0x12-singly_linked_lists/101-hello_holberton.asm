6e7f7cd69a70eae0066e913bd832f3fbbc276e67
nnachi_chima <chimakingsley216@gmail.com>
2022-08-11 09:36:47 -0700

    advanced tasks done

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+	global 	main
+	extern 	printf
+
+	section .text
+main:
+	push	rbp
+
+	mov	rdi, format
+	mov	rsi, message
+	mov	rax, 0
+	call	printf
+
+	pop	rbp
+
+	mov	rax, 0
+	ret
+
+section .date
+	message: db "Hello, Holberton", 0
+	format: db "%s", 10, 0
