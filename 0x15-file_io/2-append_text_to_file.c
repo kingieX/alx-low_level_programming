f4501cd46629b4c150e277b958125695787007ef
nnachi_chima <chimakingsley216@gmail.com>
2022-08-22 08:48:53 -0700

    create file and append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
 
 /**
  * _strlen - function that finds the length of a string
- * @str: pointer to the string
+ * @str: A pointer to the string
  * Return: length of the string
  */
 size_t _strlen(char *str)
@@ -19,19 +19,19 @@ size_t _strlen(char *str)
 }
 
 /**
- * create_file - function that creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - function that appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len = 0;
+	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
